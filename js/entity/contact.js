93c5df61d9cb6bd5d34eab57c6b360a07c994b07
Elodvd <elo@davml.org>
2021-05-19 10:29:50 +0200

    version finale

--- js/entity/contact.js
+++ js/entity/contact.js
@@ -3,7 +3,7 @@ class Contact{
     constructor(firstName, lastName, address, city, email){
       this.firstName=firstName;
       this.lastName=lastName;
-      this.address=adresse;
+      this.address=address;
       this.city=city;
       this.email=email;
     }
